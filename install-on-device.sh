13710675cdecc2948d5f28c63d341e771f3f3989
DawnDIY <chenglu1990@gmail.com>
2016-05-24 02:35:25 +0800

    new build script using python3

--- install-on-device.sh
+++
@@ -1,6 +0,0 @@
-./build-click-package.sh shadowsocks ubuntu-sdk-15.04 vivid
-CLICK_PATH=$(find ./ -name "*.click")
-echo $CLICK_PATH
-
-adb push $CLICK_PATH /home/phablet
-adb shell pkcon install-local --allow-untrusted $CLICK_PATH
